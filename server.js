3813f8ba2b0458f15244c8d7df5bfbc15172b11c
Asheela Magwili <amagwili1@mail.csuchico.edu>
2020-09-17 23:59:36 -0700

    Added survey schema, routes, and view page

--- server.js
+++ server.js
@@ -3,17 +3,22 @@ if (process.env.NODE_ENV !== 'production') {
     require('dotenv').config()
 }
 
+const mongoose = require('mongoose')
 const express = require('express')
 const app = express()
 const expressLayouts = require('express-ejs-layouts')
+const bodyParser = require('body-parser')
+
+// Import routes
 const indexRouter = require('./routes/index')
-const mongoose = require('mongoose')
+const surveysRouter = require('./routes/surveys')
 
 app.set('view engine', 'ejs')
 app.set('views', __dirname + '/views')
 app.set('layout', 'layouts/layout')
 app.use(expressLayouts)
 app.use(express.static('public'))
+app.use(bodyParser.urlencoded({ limit: '10mb', extended: false}))
 
 // Connect to DB
 mongoose.connect(process.env.DATABASE_URL, {useNewUrlParser: true, useUnifiedTopology: true})
@@ -23,6 +28,7 @@ db.once('open', error => console.log('Connected to Mongoose'))
 
 // Use routes/
 app.use('/', indexRouter)
+app.use('/surveys', surveysRouter)
 
 // For development default to port 3000
 app.listen(process.env.PORT || 3000)
