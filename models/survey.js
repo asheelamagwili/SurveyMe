4305be0199bae5c99d0d039627c94c1f4a0fa92a
Asheela Magwili <amagwili1@mail.csuchico.edu>
2020-09-19 22:58:08 -0700

    Takes user input & applies search bar of surveys

--- models/survey.js
+++ models/survey.js
@@ -1,7 +1,10 @@
 const mongoose = require('mongoose')
 
 const surveySchema = new mongoose.Schema({
-    name: String,
+    name: {
+        type: String,
+        required: true
+    },
     description: String,
     isOpen: Boolean,
     startDate: Date,
--- routes/surveys.js
+++ routes/surveys.js
@@ -4,8 +4,25 @@ const router = express.Router()
 const Survey = require('../models/survey')
 
 // All surveys route
-router.get('/', (req, res) => {
-    res.render('surveys/index')
+router.get('/', async (req, res) => {
+    let searchOptions = {}
+    // Check if the name given from the search is not null & not empty
+    if(req.query.name !== '' && req.query.name != null) {
+        // Set as a regex
+        // i - case insensitive flag
+        searchOptions.name = new RegExp(req.query.name, 'i')
+    }
+
+    try {
+        // Gets all the results of the search
+        const surveys = await Survey.find(searchOptions)
+        res.render('surveys/index', {
+            surveys: surveys,
+            searchOptions: req.query
+        })
+    } catch (err) {
+        res.redirect('/')
+    }
 })
 
 // New survey route
@@ -15,22 +32,22 @@ router.get('/new', (req,res) => {
 })
 
 // Create survey route
-router.post('/', (req,res) => {
+router.post('/', async (req,res) => {
     const survey = new Survey({
         name: req.body.name
     })
-    survey.save((err, newSurvey) => {
-        if(err) {
-            res.render('surveys/new', {
-                survey: survey,
-                errorMessage: 'Error creating survey'
-            })
-        }
-        else {
-            //res.redirect(`surveys/${newSurvey.id}`)
-            res.redirect(`surveys`)
-        }
-    })
+
+    try {
+        // Wait for survey.save() to finish then populate newSurvey
+        const newSurvey = await survey.save();
+        //res.redirect(`surveys/${newSurvey.id}`)
+        res.redirect(`surveys`)
+    } catch {
+        res.render('surveys/new', {
+            survey: survey,
+            errorMessage: 'Error creating Survey'
+        })
+    }
 })
 
 module.exports = router
